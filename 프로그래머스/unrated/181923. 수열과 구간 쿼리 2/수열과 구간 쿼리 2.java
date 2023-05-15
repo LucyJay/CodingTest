acce10b57433d7eed2e24fdcf9b5cec6ae2992f1
Hayoung Joung <110400123+LucyJay@users.noreply.github.com>
2023-05-15 15:15:26 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 2.48 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        for(int i = 0; i < queries.length; i++) {
+            int[] q = queries[i];
+            int min = -1;
+            for(int j = q[0]; j <= q[1]; j++) {
+                if(arr[j] > q[2]) {
+                    if(min == -1)
+                        min = arr[j];
+                    else if(min > arr[j])
+                        min = arr[j];
+                }
+            }
+            answer[i] = min;
+        }
+        return answer;
+    }
+}
